e05052bac7508fee45cd87324001d8f61b72c31a
prateek2408 <prateek.khushalani@gmail.com>
2024-06-16 15:06:33 +0000

    Add recursive solution

---
+++ fibonacci/recursive_main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+uint64_t fib(const uint64_t& n){
+    if (n <= 1){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    time_t start, end;
+    time(&start);
+    std::cout << fib(90) << std::endl;
+    time(&end);
+    std::cout << double(end-start) << std::endl;
+    return 0;
+}
