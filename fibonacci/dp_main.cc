971b40a01ec473358cfe4aaac80b9f367c4b3e9e
prateek2408 <prateek.khushalani@gmail.com>
2024-06-16 15:41:40 +0000

    Adding dp solution

--- fibonacci/dp_main.cc
+++ fibonacci/dp_main.cc
@@ -5,12 +5,12 @@ uint64_t fib(const uint64_t& n){
     if (n==0){
         return n;
     }
-    std::vector<uint64_t> dp(n, 0);
+    std::vector<uint64_t> dp(n+1, 0);
     int i;
     dp[0] = 0;
     dp[1] = 1;
     for (i=2;i<=n;i++){
-        dp[i+1] = dp[i-1] + dp[i-2];
+        dp[i] = dp[i-1] + dp[i-2];
     }
     return dp[n];
 }
@@ -18,7 +18,7 @@ uint64_t fib(const uint64_t& n){
 int main(){
     time_t start, end;
     time(&start);
-    std::cout << fib(2) << std::endl;
+    std::cout << fib(48) << std::endl;
     time(&end);
     std::cout << double(end-start) << std::endl;
     return 0;
